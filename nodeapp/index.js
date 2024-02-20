f403a9e67cdf01ad9b23778c2fffdc22b2e4e115
vishal <vishalkumar8673@gmail.com>
2024-02-20 15:49:53 +0530

    input

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-
+require('dotenv').config()
+console.log(process.env.PORT , "PORT")
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/test', async(req, res)=> {
-    return res.status(200).send({ status: 'ok', msg: "Jenkins Working......... 👍👍❤️"})
+    return res.status(200).send({ status: 'ok', msg: "Jenkins Working......... 👍👍❤️ 😒"})
 })
-app.listen(3001, ()=> {
-    console.log(`App is running successfully on port 3001`)
+app.listen(PORT, ()=> {
+    console.log(`App is running successfully on port ${PORT}`)
 })
